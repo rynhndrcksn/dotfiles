dac8c7c7625271fc836e7015455670e3159f7145
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-04-13 13:29:30 -0700

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -3,15 +3,20 @@
 # Filename: install_packages.sh
 # Install all the packages located in packages.list
 
-printf '\n<~ Running `sudo zypper update` before installing packages ~>\n'
-sudo zypper update
+printf '\n<~ Running `sudo zypper refresh && sudo zypper dist-upgrade` before installing packages ~>\n'
+sudo zypper ref
+sudo zypper dist-upgrade
+
+#printf '\n<~ Running `sudo zypper update` before installing packages ~>\n'
+#sudo zypper update
+
 while true; do
-    printf 'These are the packages that will be installed from scripts/packages.list:\n'
-    cat packages.list
+    printf '\n<~ These are the packages that will be installed from scripts/packages.list: ~>\n'
+    cat scripts/packages.list
     printf '\nContinue with installation? (y/n) '
     read -r opt
     case $opt in
-        [Yy]* ) cat packages.list | xargs sudo zypper --non-interactive install; break;;
+        [Yy]* ) cat scripts/packages.list | xargs sudo zypper --non-interactive install; break;;
         [Nn]* ) exit;;
         * ) printf "\nPlease answer yes or no.\n";;
     esac
