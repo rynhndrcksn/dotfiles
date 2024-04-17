1fcb188a2df9ddc8a06e6b13789f4c2f26e8dc80
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-04-16 20:16:22 -0700

    Add install.sh
    Properly installs and configures everything!

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/sh
+# Author: Ryan Hendrickson
+# Filename: install.sh
+# Displays options for the installer to execute.
+
+while true; do
+    printf '\nThese are the available options:\n'
+    printf '\t0) Execute every command\n'
+    printf '\t1) Setup symlinks\n'
+    printf '\t2) Install packages\n'
+    printf '\t3) Setup n, a node version manager\n'
+    printf '\tQ) Exit the installer\n'
+    printf '\nPlease enter your selection: '
+    read -r opt
+    case $opt in
+        [0]* ) sh scripts/everything.sh; continue;;
+        [1]* ) sh scripts/setup_symlinks.sh; continue;;
+        [2]* ) sh scripts/install_packages.sh; continue;;
+        [3]* ) sh scripts/setup_n.sh; continue;;
+        [Qq]* ) printf '\nA terminal restart may be necessary for changes to take affect\n'; exit;;
+        * ) printf '\n\nPlease enter a valid option\n';;
+    esac
+done
