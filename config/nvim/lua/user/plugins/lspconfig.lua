901d5d38ff5d2013fad86256e86f0f0b4660b0e1
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2025-12-06 16:27:33 -0800

    feat: update config/ directory

--- config/nvim/lua/user/plugins/lspconfig.lua
+++ config/nvim/lua/user/plugins/lspconfig.lua
@@ -78,6 +78,12 @@ return {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
+    -- configure ansible server
+    lspconfig["ansiblels"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
     -- configure bash server
     lspconfig["bashls"].setup({
       capabilities = capabilities,
@@ -142,14 +148,40 @@ return {
       -- root_dir = lspconfig.util.root_pattern("./*.php", "composer.json", ".git"),
     })
 
+    -- configure rust_analyzer server
+    lspconfig["rust_analyzer"].setup({
+      capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+      end,
+      settings = {
+        ["rust_analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+      }
+    })
+
     -- configure tailwindcss server
     lspconfig["tailwindcss"].setup({
       capabilities = capabilities,
       on_attach = on_attach,
     })
 
-    -- configure tsserver server
-    lspconfig["tsserver"].setup({
+    -- configure ts_ls server
+    lspconfig["ts_ls"].setup({
       capabilities = capabilities,
       on_attach = on_attach,
     })
